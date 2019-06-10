4bd19e050719c937ab4145a72935c30c799ae210
platoneko <platonekosama@gmail.com>
2019-06-10 17:30:27 +0800

    add transformer_ffn

--- transformer/transformer_ffn.py
+++ transformer/transformer_ffn.py
@@ -3,11 +3,11 @@ import torch.nn.functional as F
 
 
 class TransformerFFN(nn.Module):
-    def __init__(self, model_size, hidden_size, dropout=0.0):
+    def __init__(self, model_dim, hidden_size, dropout=0.0):
         super(TransformerFFN, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
-        self.lin1 = nn.Linear(model_size, hidden_size)
-        self.lin2 = nn.Linear(hidden_size, model_size)
+        self.lin1 = nn.Linear(model_dim, hidden_size)
+        self.lin2 = nn.Linear(hidden_size, model_dim)
         nn.init.xavier_uniform_(self.lin1.weight)
         nn.init.xavier_uniform_(self.lin2.weight)
         # TODO: initialize biases to 0
